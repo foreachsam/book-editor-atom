445ede266eb7e04d7ed7418ef8669ff2ec55073e
foreachsam <foreachsam@users.noreply.github.com>
2015-07-20 17:34:03 +0800

    add atom-install.sh

--- book/resource/integration/atom-install.sh
+++ book/resource/integration/atom-install.sh
@@ -1,1 +1,6 @@
- 
+#!/bin/sh
+
+Service_Dir=~/.kde/share/kde4/services/ServiceMenus/
+mkdir $Service_Dir -p
+cd $Service_Dir
+wget -c https://foreachsam.github.io/book-editor-atom/book/resource/integration/atom.desktop
